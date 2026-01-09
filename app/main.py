c18292d07f128c6d45ad21fcc29a6d35b1ab0f7f
nazarkravets2007 <nazarfootballer11@gmail.com>
2026-01-09 11:45:53 +0200

    Update main.py
    fixxing

--- app/main.py
+++ app/main.py
@@ -1,16 +1,18 @@
 def format_linter_error(error: dict) -> dict:
     return {
-        "line": error["line"],
-        "column": error["column"],
-        "message": error["message"],
+        "line": error["line_number"],
+        "column": error["column_number"],
+        "message": error["text"],
         "name": error["code"],
+        "source": "flake8",
     }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
-        "filePath": file_path,
-        "messages": [format_linter_error(error) for error in errors],
+        "path": file_path,
+        "status": "failed" if errors else "passed",
+        "errors": [format_linter_error(error) for error in errors],
     }
 
 
