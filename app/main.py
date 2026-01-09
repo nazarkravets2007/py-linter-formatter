90527f97e91b3931e39c92a4b8f15a7dbd8b0713
nazarkravets2007 <nazarfootballer11@gmail.com>
2026-01-09 11:35:43 +0200

    Update main.py
    Fix order in linter report

--- app/main.py
+++ app/main.py
@@ -18,6 +18,7 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 def format_linter_report(linter_report: dict) -> list:
     return [
-        format_single_linter_file(file_path, linter_report[file_path])
-        for file_path in sorted(linter_report)
+        format_single_linter_file(file_path, errors)
+        for file_path, errors in linter_report.items()
     ]
+
